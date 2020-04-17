c7054bd76d2e5f8c000e930b28da993564cd90ff
abc <sontung2101@gmail.com>
2020-04-17 15:23:59 +0700

    new update 17/04/2020

--- fabbiBlog/blog/management/commands/feedfake.py
+++ fabbiBlog/blog/management/commands/feedfake.py
@@ -2,20 +2,21 @@ from django.core.management.base import BaseCommand, CommandError
 from blog.models import *
 from faker import Faker
 from faker.generator import random
-
+from django.utils import timezone
 fake = Faker(['en_US'])
+
+
 class Command(BaseCommand):
     help = 'Faker T1'
 
     def add_arguments(self, parser):
-        parser.add_argument('record', help='get type int', type=int)
+        parser.add_argument('record', type=int, help='get type int')
 
     def handle(self, *args, **options):
         records = options['record']
-        user = User.objects.all()
         for _ in range(0, records):
             PostModel.objects.create(
                 post_title=fake.name(),
-                user_id=random.choice(user),
-                content=fake.name(),
+                user_id='1',
+                content=fake.text(),
             )
