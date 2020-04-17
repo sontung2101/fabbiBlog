f8ad36628d33e25316f73b4d2c370eb700f335e0
abc <sontung2101@gmail.com>
2020-04-17 16:00:09 +0700

    remove idea

--- fabbiBlog/blog/admin.py
+++ fabbiBlog/blog/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 from .models import *
 
+
 # Register your models here.
 
-# class PersonAdmin(admin.ModelAdmin):
-#     model = PersonModel
-#     list_display = ['id', 'first_name', 'mid_name', 'last_name',
-#                     'is_active', 'is_deleted', 'created_at', 'department']
-#     list_filter = ['department', 'is_active']
-#     search_fields = ['first_name']
-admin.site.register(PostModel)
-admin.site.register(CommentsModel)
-# admin.site.register(PostDetailsModel)
+class PostAdmin(admin.ModelAdmin):
+    model = PostModel
+    list_display = ['post_title','is_active', 'is_deleted']
+    list_filter = ['user', 'post_title']
+    search_fields = ['post_title']
+    readonly_fields = ['is_active', 'is_deleted']
+
+
+class CommentsAdmin(admin.ModelAdmin):
+    model = CommentsModel
+    list_display = ['comment', 'is_deleted', 'is_active']
+    list_filter = ['comment']
+    search_fields = ['comment']
+    readonly_fields = ['is_active', 'is_deleted']
+
+admin.site.register(PostModel, PostAdmin)
+admin.site.register(CommentsModel,CommentsAdmin)
