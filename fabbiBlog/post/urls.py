35a2e0be9f283efd6ddfb0ebd1f3ecf286b511f2
Nguyễn Sơn Tùng <56378072+sontung2101@users.noreply.github.com>
2020-04-27 13:35:21 +0700

    add template admin

--- fabbiBlog/post/urls.py
+++ fabbiBlog/post/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('index', index, name='index'),
+    path('', index, name='index'),
     path('post', post, name='post'),
+    path('login', login, name='login'),
+    path('register', register, name='register'),
 ]
