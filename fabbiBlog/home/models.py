dc0537640b627028c1a3412671f4a7d56d699a99
minhdang0598 <61687214+minhdang0598@users.noreply.github.com>
2020-05-08 10:10:10 +0700

    Update models.py

--- fabbiBlog/home/models.py
+++ fabbiBlog/home/models.py
@@ -1,29 +1,52 @@
 from django.db import models
 from django.contrib.auth.models import User
+from adminBlog.models import *
+from tinymce.models import HTMLField
+from adminBlog.models import *
 
 
 # Create your models here.
 # Create your models here.
+
+class CategoryModel(models.Model):
+    title = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Category'
+
+    def __str__(self):
+        return self.title
+
 
 class PostModel(models.Model):
-    post_title = models.CharField(max_length=100, unique=True)
-    content = models.CharField(max_length=500, null=True, blank=True)
+    title = models.CharField(max_length=100, unique=True)
+    sapo = models.CharField(max_length=200,blank=True)
+    content = HTMLField()
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    thumbnail = models.ImageField(upload_to='uploads', blank=False)
+    categories = models.ManyToManyField(CategoryModel)
     is_active = models.BooleanField(null=True, blank=True, default=True)
     is_deleted = models.BooleanField(null=True, blank=True, default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Post'
 
+    def __str__(self):
+        return self.title
+
 
 class CommentsModel(models.Model):
-    content = models.CharField(max_length=255, null=True, blank=True)
-    comment_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(myUser, on_delete=models.CASCADE)
+    content = models.CharField(max_length=100,blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(PostModel, related_name='comments', on_delete=models.CASCADE)
     is_active = models.BooleanField(null=True, blank=True, default=True)
     is_deleted = models.BooleanField(null=True, blank=True, default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = 'Comments'
+
+    def __str__(self):
+        return self.user
