c719482b6a27f377d7e47a583154bb10e202f406
minhdang0598 <61687214+minhdang0598@users.noreply.github.com>
2020-05-08 10:11:12 +0700

    Update views.py

--- fabbiBlog/home/views.py
+++ fabbiBlog/home/views.py
@@ -1,23 +1,10 @@
-from django.shortcuts import render, HttpResponse, redirect
-from .models import PostModel, CommentsModel
-import requests
+from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
 def index(request):
-    url = ""
-    data = requests.get(url).json()
-    return render(request, '../templates/home/index.html', {'Posts': data})
+    pass
 
 
 def post(request):
-    url = ""
-    data = requests.get(url+"/"+id).json()
-    comments = requests.get(url+"/"+id).json()
-    if request.method == "POST":
-        if "add_comment" in request.POST:
-            content = request.POST["content"]
-            new_comment = CommentsModel(content=content)
-            requests.post(url, new_comment.json())
-            return redirect("/")
-    return render(request, '../templates/home/post_detail.html', {'Post': data}, {'Comments': comments})
+    pass
